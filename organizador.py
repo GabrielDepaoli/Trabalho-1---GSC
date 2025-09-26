4f28a5747edf4d5d0d0d6801f368142003a60681
GabrielDepaoli <depaoli.gab3@gmail.com>
2025-09-26 18:51:59 -0300

    Adição da função de editar tarefas

--- organizador.py
+++ organizador.py
@@ -5,7 +5,8 @@ def mostrar_menu():
     print("1 - Adicionar tarefa")
     print("2 - Ver tarefas")
     print("3 - Remover tarefa")
-    print("4 - Sair")
+    print("4 - Editar tarefa")
+    print("5 - Sair")
 
 while True:
     mostrar_menu()
@@ -15,6 +16,7 @@ while True:
         tarefa = input("Digite a tarefa: ")
         tarefas.append(tarefa)
         print("Tarefa adicionada com sucesso!")
+
     elif opcao == "2":
         if len(tarefas) == 0:
             print("Nenhuma tarefa cadastrada.")
@@ -22,6 +24,7 @@ while True:
             print("\nLista de tarefas:")
             for i, t in enumerate(tarefas, start=1):
                 print(f"{i}. {t}")
+
     elif opcao == "3":
         if len(tarefas) == 0:
             print("Não há tarefas para remover.")
@@ -35,8 +38,25 @@ while True:
                 print(f"Tarefa '{removida}' removida com sucesso!")
             else:
                 print("Número inválido.")
+
     elif opcao == "4":
+        if len(tarefas) == 0:
+            print("Não há tarefas para editar.")
+        else:
+            print("\nTarefas:")
+            for i, t in enumerate(tarefas, start=1):
+                print(f"{i}. {t}")
+            indice = int(input("Digite o número da tarefa que deseja editar: "))
+            if 1 <= indice <= len(tarefas):
+                nova_tarefa = input("Digite a nova descrição da tarefa: ")
+                tarefas[indice - 1] = nova_tarefa
+                print("Tarefa atualizada com sucesso!")
+            else:
+                print("Número inválido.")
+
+    elif opcao == "5":
         print("Finalizando...")
         break
+
     else:
         print("Opção inválida, tente novamente.")
